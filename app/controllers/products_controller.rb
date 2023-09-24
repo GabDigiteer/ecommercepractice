35a1cc1129d66b30e7e3fc852b1ed4c38bac973b
GabDigiteer <gabriel.digiteer@gmail.com>
2023-09-24 13:24:32 +0800

    updated product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,12 @@
 class ProductsController < ApplicationController
+
   def index
-    @products = Product.all
+    @products = pagy(Product.all)
   end
 
   def new
     @product = Product.new
-    @products = Product.all
+    @pagy, @products = pagy(Product.all)
   end
 
   def show
@@ -19,6 +20,23 @@ class ProductsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+  
+  def update
+    @product = Product.find(params[:id])
+    debugger
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   private
