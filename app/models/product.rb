07e452e8c0fad18d3fbfd5b25b6b78c410c9eab8
GabDigiteer <gabriel.digiteer@gmail.com>
2023-10-02 12:09:28 +0800

    Fixed destroy product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ApplicationRecord
 
-    has_many :cart_items
+    has_many :cart_items, dependent: :destroy
+    has_many :purchases, dependent: :destroy
     has_many :users, through: :cart_items
+
 
     validates :name, presence: true
     validates :description, presence: true
